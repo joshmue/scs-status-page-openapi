6dec723c5e617db330934859197d3bd481249cbd
Joshua Mühlfort <muehlfort@gonicus.de>
2022-12-21 15:46:30 +0000

    Refactor ensureProjectConfiguration using graphql aliases
    Signed-off-by: Joshua Mühlfort <muehlfort@gonicus.de>

--- ensure_config.go
+++ ensure_config.go
@@ -12,13 +12,18 @@ func (s *ServerImplementation) ensureProjectConfiguration() error {
 	var query struct {
 		User struct {
 			ProjectV2 struct {
-				Field struct {
+				StatusField struct {
 					ProjectV2SingleSelectField struct {
 						Options []struct {
 							Name string
 						}
 					} `graphql:"... on ProjectV2SingleSelectField"`
-				} `graphql:"field(name: $status)"`
+				} `graphql:"status: field(name: $status)"`
+				BeganAtField struct {
+					ProjectV2Field struct {
+						DataType string
+					} `graphql:"... on ProjectV2Field"`
+				} `graphql:"beganat: field(name: $beganat)"`
 			} `graphql:"projectV2(number: $number)"`
 		} `graphql:"user(login: $user)"`
 	}
@@ -26,46 +31,26 @@ func (s *ServerImplementation) ensureProjectConfiguration() error {
 		context.Background(),
 		&query,
 		map[string]interface{}{
-			"user":   githubv4.String(s.ProjectOwner),
-			"number": githubv4.Int(s.ProjectNumber),
-			"status": githubv4.String("Status"),
-		},
-	)
-	if err != nil {
-		return err
-	}
-	options := query.User.ProjectV2.Field.ProjectV2SingleSelectField.Options
-	if options[len(options)-1].Name != s.LastPhase {
-		return fmt.Errorf("expected final phase to be %s; is %s", s.LastPhase, options[len(options)-1].Name)
-	}
-
-	// Check "Began At"
-	var queryBeganAt struct {
-		User struct {
-			ProjectV2 struct {
-				Field struct {
-					ProjectV2Field struct {
-						DataType string
-					} `graphql:"... on ProjectV2Field"`
-				} `graphql:"field(name: $beganat)"`
-			} `graphql:"projectV2(number: $number)"`
-		} `graphql:"user(login: $user)"`
-	}
-	err = s.GithubV4Client.Query(
-		context.Background(),
-		&queryBeganAt,
-		map[string]interface{}{
 			"user":    githubv4.String(s.ProjectOwner),
 			"number":  githubv4.Int(s.ProjectNumber),
+			"status":  githubv4.String("Status"),
 			"beganat": githubv4.String("Began At"),
 		},
 	)
 	if err != nil {
 		return err
 	}
-	beganAtDataType := queryBeganAt.User.ProjectV2.Field.ProjectV2Field.DataType
-	if beganAtDataType != "TEXT" {
-		return fmt.Errorf(`expected "Began At" data type to be TEXT; is %s`, beganAtDataType)
+	// Check "Status" field
+	options := query.User.ProjectV2.StatusField.ProjectV2SingleSelectField.Options
+	if len(options) == 0 {
+		return fmt.Errorf(`expected to have phases encoded as fields of "Status"; not having any`)
+	}
+	if options[len(options)-1].Name != s.LastPhase {
+		return fmt.Errorf(`expected final phase to be "%s"; is "%s"`, s.LastPhase, options[len(options)-1].Name)
+	}
+	// Check "Began At" field
+	if query.User.ProjectV2.BeganAtField.ProjectV2Field.DataType != "TEXT" {
+		return fmt.Errorf(`expected field "Began At" to be "TEXT"; is "%s"`, query.User.ProjectV2.BeganAtField.ProjectV2Field.DataType)
 	}
 	return nil
 }
