d448b7e08025909a84d74162fa367caa7a6fd837
Joshua Mühlfort <muehlfort@gonicus.de>
2022-12-21 13:26:53 +0000

    Ensure final phase being set to expected value
    Signed-off-by: Joshua Mühlfort <muehlfort@gonicus.de>

--- main.go
+++ main.go
@@ -23,7 +23,40 @@ type ServerImplementation struct {
 	ProjectNumber  int64
 	ProjectID      string
 	ImpactTypes    []string
-	Phases         []string
+	LastPhase      string
+}
+
+func (s *ServerImplementation) ensureProjectConfiguration() error {
+	var query struct {
+		User struct {
+			ProjectV2 struct {
+				Field struct {
+					ProjectV2SingleSelectField struct {
+						Options []struct {
+							Name string
+						}
+					} `graphql:"... on ProjectV2SingleSelectField"`
+				} `graphql:"field(name: $status)"`
+			} `graphql:"projectV2(number: $number)"`
+		} `graphql:"user(login: $user)"`
+	}
+	err := s.GithubV4Client.Query(
+		context.Background(),
+		&query,
+		map[string]interface{}{
+			"user":   githubv4.String(s.ProjectOwner),
+			"number": githubv4.Int(s.ProjectNumber),
+			"status": githubv4.String("Status"),
+		},
+	)
+	options := query.User.ProjectV2.Field.ProjectV2SingleSelectField.Options
+	if options[len(options)-1].Name != s.LastPhase {
+		return fmt.Errorf("expected final phase to be %s; is %s", s.LastPhase, options[len(options)-1].Name)
+	}
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *ServerImplementation) fillProjectID() error {
@@ -97,6 +130,7 @@ func main() {
 	projectOwner := flag.String("github.project.user", "joshmue", "user owning the project")
 	projectNumber := flag.Int64("github.project.number", 1, "project number")
 	impactTypeList := flag.String("impacttypes", "performance-degration,connectivity-issues", `","-seperated list of impact types`)
+	lastPhase := flag.String("last-phase", "Done", "last phase of incidents")
 	flag.Parse()
 
 	httpClient := oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(
@@ -108,8 +142,12 @@ func main() {
 		ProjectOwner:   *projectOwner,
 		ProjectNumber:  *projectNumber,
 		ImpactTypes:    strings.Split(*impactTypeList, ","),
+		LastPhase:      *lastPhase,
 	}
 	if err := server.fillProjectID(); err != nil {
+		log.Fatalln(err)
+	}
+	if err := server.ensureProjectConfiguration(); err != nil {
 		log.Fatalln(err)
 	}
 	e := echo.New()
