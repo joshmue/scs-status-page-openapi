83754e347dafcfddec5b1cd12f20652e1794c359
Joshua Mühlfort <muehlfort@gonicus.de>
2022-12-21 15:56:48 +0000

    Put handlers into another file
    Signed-off-by: Joshua Mühlfort <muehlfort@gonicus.de>

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -24,70 +23,6 @@ type ServerImplementation struct {
 	ProjectID      string
 	ImpactTypes    []string
 	LastPhase      string
-}
-
-func (s *ServerImplementation) fillProjectID() error {
-	var query struct {
-		User struct {
-			ProjectV2 struct {
-				Id string `graphql:"id"`
-			} `graphql:"projectV2(number: $number)"`
-		} `graphql:"user(login: $user)"`
-	}
-	err := s.GithubV4Client.Query(
-		context.Background(),
-		&query,
-		map[string]interface{}{
-			"user":   githubv4.String(s.ProjectOwner),
-			"number": githubv4.Int(s.ProjectNumber),
-		},
-	)
-	if err != nil {
-		return err
-	}
-	s.ProjectID = query.User.ProjectV2.Id
-	return nil
-}
-
-func (s *ServerImplementation) GetComponents(ctx echo.Context) error {
-	return fmt.Errorf("not implemented")
-}
-func (s *ServerImplementation) GetImpacttypes(ctx echo.Context) error {
-	return fmt.Errorf("not implemented")
-}
-func (s *ServerImplementation) GetIncidents(ctx echo.Context, params api.GetIncidentsParams) error {
-	return fmt.Errorf("not implemented")
-}
-func (s *ServerImplementation) GetPhases(ctx echo.Context) error {
-	var query struct {
-		User struct {
-			ProjectV2 struct {
-				Field struct {
-					ProjectV2SingleSelectField struct {
-						Options []struct {
-							Name string
-						}
-					} `graphql:"... on ProjectV2SingleSelectField"`
-				} `graphql:"field(name: \"Status\")"`
-			} `graphql:"projectV2(number: $number)"`
-		} `graphql:"user(login: $user)"`
-	}
-	err := s.GithubV4Client.Query(
-		context.Background(),
-		&query,
-		map[string]interface{}{
-			"user":   githubv4.String(s.ProjectOwner),
-			"number": githubv4.Int(s.ProjectNumber),
-		},
-	)
-	if err != nil {
-		return err
-	}
-	phases := []api.IncidentPhase{}
-	for _, phase := range query.User.ProjectV2.Field.ProjectV2SingleSelectField.Options {
-		phases = append(phases, phase.Name)
-	}
-	return ctx.JSON(200, phases)
 }
 
 func main() {
