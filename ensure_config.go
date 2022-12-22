80493f8d014eaa33488641743725fe6b60f1153d
Joshua Mühlfort <muehlfort@gonicus.de>
2022-12-22 07:53:57 +0000

    Use fetched projectid in ensureProjectConfiguration
    Signed-off-by: Joshua Mühlfort <muehlfort@gonicus.de>

--- ensure_config.go
+++ ensure_config.go
@@ -11,7 +11,7 @@ import (
 func (s *ServerImplementation) ensureProjectConfiguration() error {
 	// Check final "Status" field
 	var query struct {
-		User struct {
+		Node struct {
 			ProjectV2 struct {
 				Repositories struct {
 					Nodes []struct {
@@ -46,15 +46,14 @@ func (s *ServerImplementation) ensureProjectConfiguration() error {
 						DataType string
 					} `graphql:"... on ProjectV2Field"`
 				} `graphql:"endedat: field(name: \"Ended At\")"`
-			} `graphql:"projectV2(number: $number)"`
-		} `graphql:"user(login: $user)"`
+			} `graphql:"... on ProjectV2"`
+		} `graphql:"node(id: $projectid)"`
 	}
 	err := s.GithubV4Client.Query(
 		context.Background(),
 		&query,
 		map[string]interface{}{
-			"user":   githubv4.String(s.ProjectOwner),
-			"number": githubv4.Int(s.ProjectNumber),
+			"projectid": githubv4.ID(s.ProjectID),
 		},
 	)
 	if err != nil {
@@ -62,7 +61,7 @@ func (s *ServerImplementation) ensureProjectConfiguration() error {
 	}
 	// Check components
 	componentFound := false
-	for _, repo := range query.User.ProjectV2.Repositories.Nodes {
+	for _, repo := range query.Node.ProjectV2.Repositories.Nodes {
 		for _, label := range repo.Labels.Nodes {
 			if strings.HasPrefix(label.Name, "component:") {
 				componentFound = true
@@ -73,7 +72,7 @@ func (s *ServerImplementation) ensureProjectConfiguration() error {
 		return fmt.Errorf("expected components, got none")
 	}
 	// Check "Status" field
-	phaseOptions := query.User.ProjectV2.StatusField.ProjectV2SingleSelectField.Options
+	phaseOptions := query.Node.ProjectV2.StatusField.ProjectV2SingleSelectField.Options
 	if len(phaseOptions) == 0 {
 		return fmt.Errorf(`expected to have phases encoded as fields of "Status"; not having any`)
 	}
@@ -81,17 +80,17 @@ func (s *ServerImplementation) ensureProjectConfiguration() error {
 		return fmt.Errorf(`expected final phase to be "%s"; is "%s"`, s.LastPhase, phaseOptions[len(phaseOptions)-1].Name)
 	}
 	// Check "Impact Type" field
-	impactTypeOptions := query.User.ProjectV2.ImpactTypeField.ProjectV2SingleSelectField.Options
+	impactTypeOptions := query.Node.ProjectV2.ImpactTypeField.ProjectV2SingleSelectField.Options
 	if len(impactTypeOptions) == 0 {
 		return fmt.Errorf(`expected to have impact types encoded as fields of "Impact Type"; not having any`)
 	}
 	// Check "Began At" field
-	if query.User.ProjectV2.BeganAtField.ProjectV2Field.DataType != "TEXT" {
-		return fmt.Errorf(`expected field "Began At" to be "TEXT"; is "%s"`, query.User.ProjectV2.BeganAtField.ProjectV2Field.DataType)
+	if query.Node.ProjectV2.BeganAtField.ProjectV2Field.DataType != "TEXT" {
+		return fmt.Errorf(`expected field "Began At" to be "TEXT"; is "%s"`, query.Node.ProjectV2.BeganAtField.ProjectV2Field.DataType)
 	}
 	// Check "Ended At" field
-	if query.User.ProjectV2.EndedAtField.ProjectV2Field.DataType != "TEXT" {
-		return fmt.Errorf(`expected field "Began At" to be "TEXT"; is "%s"`, query.User.ProjectV2.EndedAtField.ProjectV2Field.DataType)
+	if query.Node.ProjectV2.EndedAtField.ProjectV2Field.DataType != "TEXT" {
+		return fmt.Errorf(`expected field "Began At" to be "TEXT"; is "%s"`, query.Node.ProjectV2.EndedAtField.ProjectV2Field.DataType)
 	}
 	return nil
 }
