b6692dfa6816fec6081c2069ced3361af8b46ccb
Joshua Mühlfort <muehlfort@gonicus.de>
2023-01-02 14:44:42 +0000

    Implement GetComponent
    Signed-off-by: Joshua Mühlfort <muehlfort@gonicus.de>

--- api_implementation.go
+++ api_implementation.go
@@ -41,7 +41,41 @@ func (s *ServerImplementation) fillProjectID() error {
 }
 
 func (s *ServerImplementation) GetComponent(ctx echo.Context, componentId string) error {
-	return fmt.Errorf("not implemented")
+	var query struct {
+		Node struct {
+			Label struct {
+				Id     string
+				Name   string
+				Issues struct {
+					Nodes []struct {
+						Id string
+					}
+				} `graphql:"issues(first:10)"`
+			} `graphql:"... on Label"`
+		} `graphql:"node(id: $labelid)"`
+	}
+	err := s.GithubV4Client.Query(
+		context.Background(),
+		&query,
+		map[string]interface{}{
+			"labelid": githubv4.ID(componentId),
+		},
+	)
+	if err != nil {
+		ctx.Logger().Error(err)
+		return echo.NewHTTPError(500)
+	}
+	affectedBy := []api.Id{}
+	for i := range query.Node.Label.Issues.Nodes {
+		affectedBy = append(affectedBy, query.Node.Label.Issues.Nodes[i].Id)
+	}
+	component := api.Component{
+		AffectedBy:  affectedBy,
+		DisplayName: strings.TrimPrefix(query.Node.Label.Name, "component:"),
+		Id:          componentId,
+		Labels:      map[string]string{}, // TODO
+	}
+	return ctx.JSON(200, component)
 }
 func (s *ServerImplementation) GetComponents(ctx echo.Context) error {
 	var query struct {
